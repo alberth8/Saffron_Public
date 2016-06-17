e081aa7564575a00640112502d0ec9ac1579fe94
Shane Hubbell <shane.hubbell@gmail.com>
2016-06-17 11:56:41 -0700

    FEAT setup store

--- client/app.js
+++ client/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import makeStore from './src/makeStore.js';
+
+const store = makeStore();
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <Provider store={store}>
+          <h1>Hello, world!</h1>
+        </Provider>
       </div>
     )
   }
