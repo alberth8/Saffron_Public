0816f53853ecc41e431cb74f0fa804aa55e86cd7
Shane Hubbell <shane.hubbell@gmail.com>
2016-06-16 17:27:44 -0700

    FEAT basic node and express server

---
+++ server/config/middleware.js
@@ -0,0 +1,20 @@
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// Good for once we add webpack
+// const webpackConfig = require('webpack.config.js');
+// const webpack = require('webpack');
+// const compiler = webpack(webpackConfig);
+// const webpackDevMiddleware = require('webpack-dev-middleware');
+// const webpackHotMiddleware = require('webpack-hot-middlware');
+
+module.exports = function (app, express) {
+	// app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: webpackConfig.output,publicPath}))
+	// app.use(webpackDevMiddleware(compiler))
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+
+	// MAY NEED TO EDIT THE STATIC FILE ADDRESS
+	// app.use(express.static(__dirname + '/../../client/public'))
+}
