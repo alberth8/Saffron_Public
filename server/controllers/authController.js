49544a16465b59928c9b67113b746f228f424c9d
Daniel Rabinovich <drabin89@gmail.com>
2016-06-21 14:26:25 -0700

    Set up creating a new user and salt/hashing password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,6 @@
 const User = require('../models/user.js');
 const Promise = require('bluebird');
-// for auth:
-// const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 
 
 // TODO: will complete when OAuth is implemented
@@ -11,7 +10,25 @@ module.exports = {
     let userInfo = {
       email: req.body.username,
       password: req.body.password
-    }
+    };
+
+    User.where('email' , userInfo.email).fetch().then(function(user) {
+    	if(!user) {
+    	  bcrypt.genSalt(10, function(err, salt) {
+    	  	if(err) {return err};
+    	    bcrypt.hash(userInfo.password, salt, null, function(err, hash) {
+    		  if (err) {return err};
+    		    userInfo.password = hash
+    		    return new User(userInfo).save();
+    	    })
+    	  })
+    	}
+      }).then(function(newUser) {
+    	res.status(302).send({ error: 'Email already exists'});
+    })
+    .catch(function(err) {
+    	console.log(err);
+    });
   },
 
   findUser: function (req, res) {
