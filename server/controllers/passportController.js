a1b23b4a364eb7b59b90555e36eb6a666735dcb6
Daniel Rabinovich <drabin89@gmail.com>
2016-06-21 16:13:01 -0700

    passport and bcrypt added to the backend

---
+++ server/controllers/passportController.js
@@ -0,0 +1,37 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+const User = require('../collections/users.js');
+const config = require('../config/authConfig.js');
+const bcrypt = require('bcrypt-nodejs');
+
+const localLogin = new LocalStrategy( {
+	usernameField: 'email'
+ }, function(email, password, done) {
+ 	User.where({ 'email': email}).fetch().then(function(user) {
+ 		if(!user) { return done(false) };
+ 		bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+ 			if(err) { return err; }
+
+ 			return done(true);
+ 		})
+ 	})
+})
+
+const jwtOptions = {
+	jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+	secretOrKey: config.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+	User.where('id', payload.sub).fetch().then(function(user) {
+		if (user) {
+			done(user);
+		} else {
+			done(false);
+		}
+	});
+});
+
+passport.use(jwtLogin);
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,14 @@
 const authController = require('../controllers/authController.js');
+const passportService = require('../controllers/passportController.js');
+const passport = require('passport');
+
+
+const requireAuth = passport.authenticate('jwt', { session: false});
 
 module.exports = function (app) {
-
+	app.get('/profile', function() {
+		res.send
+	})
 	app.post('api/createUser', authController.createUser);
-	
+
 }
