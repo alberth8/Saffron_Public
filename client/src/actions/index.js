bb0c6ffe526f19db00956dde85bda0a1c024d821
Albert Han <hanalbert87@gmail.com>
2016-06-23 14:21:29 -0700

    add action to post recipe

---
+++ client/src/actions/index.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+
+export function authError(error) {
+ return {
+   type: 'AUTH_ERROR',
+   payload: error,
+ };
+}
+
+export function loginUser(email, password) {
+ return (dispatch) => {
+   axios.post('/login', { email, password })
+   .then((response) => {
+     dispatch({
+       type: 'AUTH_USER',
+     });
+     localStorage.setItem('token', response.data.token);
+     browserHistory.push('/');
+   }).catch(() => {
+     dispatch(authError('Bad Login Info'));
+   });
+ };
+}
+
+export function signupUser(email, password) {
+ return () => {
+   axios.post('/api/signup', { email, password });
+ };
+}
+
+export function signoutUser() {
+ localStorage.removeItem('token');
+
+ return { type: 'UNAUTH_USER' };
+}
+
+// Post recipe to db. Used in addRecipeForm
+export function addRecipe(addRecipeObj) {
+  console.log('IN ACTIONS/INDEX.JS');
+  console.log('addRecipeObj', addRecipeObj);
+  return () => {
+    axios.post('/api/addrecipe', addRecipeObj)
+    .then(function(response) {
+        console.log('THEN > RESPONSE')
+        console.log(response);
+      }).catch(function(error) {
+        console.log('ERROR:', error);
+    });
+   }
+}
+
+// () => { // return callback
+//     console.log('IN CALLBACK');
+//     axios.post('/api/addrecipe', {
+//       recipeTitle: recipeTitle
+//       }).then(function(response) {
+//         console.log('THEN > RESPONSE')
+//         console.log(response);
+//       }).catch(function(error) {
+//         console.log('ERROR:', error);
+//       });
