ed83701a51a026e81f9828635a1055448cad6412
Albert Han <hanalbert87@gmail.com>
2016-06-24 11:51:58 -0700

    add manual join tables

--- server/db/schema.js
+++ server/db/schema.js
@@ -28,6 +28,7 @@ db.knex.schema.hasTable('users').then((exists) => {
   }
 });
 
+
 db.knex.schema.hasTable('recipes').then((exists) => {
   if (!exists) {
     db.knex.schema.createTable('recipes', (recipe) => {
@@ -53,9 +54,49 @@ db.knex.schema.hasTable('ingredients').then((exists) => {
   }
 });
 
-module.exports = db;
+// Relationship between ingredients and recipes
+db.knex.schema.hasTable('ingredients_recipes').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('ingredients_recipes', (ing_rec) => {
+      ing_rec.increments('id').primary();
+      ing_rec.integer('ingredient_id').unsigned().references('id')
+             .inTable('ingredients');
+      ing_rec.integer('recipe_id').unsigned().references('id')
+             .inTable('recipes');
+    }).then((table) => {
+      console.log('Created table `ingredients_recipes`', table);
+    });
+  }
+});
 
-// join tables: 
-//  ingredients_recipes: associates recipes w/ ingredients
-//  recipes_users: favorited table
-//  ingredients_users: user's saved sets
+// Favorited recipes
+db.knex.schema.hasTable('recipes_users').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('recipes_users', (rec_user) => {
+      rec_user.increments('id').primary();
+      rec_user.integer('recipe_id').unsigned().references('id')
+              .inTable('recipes');
+      rec_user.integer('user_id').unsigned().references('id')
+              .inTable('users');
+    }).then((table) => {
+      console.log('Created table `recipes_users`', table);
+    });
+  }
+});
+
+// User's saved sets
+db.knex.schema.hasTable('ingredients_users').then((exists) => {
+  if (!exists) {
+    db.knex.schema.createTable('ingredients_users', (rec_user) => {
+      rec_user.increments('id').primary();
+      rec_user.integer('ingredient_id').unsigned().references('id')
+              .inTable('ingredients');
+      rec_user.integer('user_id').unsigned().references('id')
+              .inTable('users');
+    }).then((table) => {
+      console.log('Created table `ingredients_users`', table);
+    });
+  }
+});
+
+module.exports = db;
