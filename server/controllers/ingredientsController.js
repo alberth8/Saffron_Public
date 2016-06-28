328d5963de872ff6b1abcb0407388912213fa3be
Shane Hubbell <shane.hubbell@gmail.com>
2016-06-28 15:49:52 -0700

    ingredients controller working but need a UID in the ingredients_users table

--- server/controllers/ingredientsController.js
+++ server/controllers/ingredientsController.js
@@ -1,9 +1,9 @@
 // import { Ingredient } from '../models/ingredient';
 // import { Ingredients } from '../collections/ingredients';
 const IngredientModel = require('../models/ingredient.js');
-// const Ingredient_UserModel = require('../models/ingredient_user.js');
+const Ingredient_UserModel = require('../models/ingredient_user.js');
 const IngredientsCollection = require('../collections/ingredients.js');
-// const Ingredient_UserCollection = require('../collections/ingredient_user.js');
+const Ingredient_UserCollection = require('../collections/ingredients_users.js');
 // let ingredientId;
 // Note to team: try to do these relationally. If not,
 // make use of the req object to obtain userID
@@ -12,7 +12,6 @@ module.exports = {
   updateIngredients: (req, res) => {
     const ingredients = req.body.selectedIngredients;
     const userId = req.body.userID;
-    console.log('ingredients controller!!!!', ingredients, userId);
     res.send('POST to /api/ingredients, this is ingredients');
     // basic steps needed
     // 1: Check to see if the user has saved any ingredient lists
@@ -38,9 +37,30 @@ module.exports = {
               .then((ingredientModel) => {
                 ingredientIdArray.push(ingredientModel.attributes.id);
                 alreadySaved = false;
-                console.log(ing, 'ing ID:', ingredientModel.attributes.id);
-                if (alreadySaved !== false) {
-                  // do something
+                console.log('Ingredient: ', ing, ' ID:', ingredientModel.attributes.id);
+                // if the user hasn't saved this ingredient list before, add it to the collection
+                if (alreadySaved === false &&
+                  ingredientIdArray.length === ingredients.length) {
+                  ingredientIdArray.forEach((ingId) => {
+                    Ingredient_UserCollection.create({
+                      ingredient_id: ingId,
+                      user_id: userId,
+                    })
+                    .then((ingridentUserModel) => {
+                      console.log('ingredient_User saved:', ingridentUserModel);
+                    })
+                    .catch((error) => { console.log(error); });
+                  });
+                } else {
+                  Ingredient_UserModel.where({ user_id: userId }).fetchAll()
+                    .then((i_u_found) => {
+                      const throwAway = [];
+                      i_u_found.models.forEach((i_u_model) => {
+                        throwAway.push(i_u_model.attributes);
+                      });
+                      console.log('throw away array: ', throwAway);
+                    })
+                    .catch((error) => { console.log(error); console.log(userId); });
                 }
               })
               .catch((error) => { console.log(error); });
