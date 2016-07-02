129fcd52ee61cfcf236bc9f616f07b2c5d766f2f
Daniel Rabinovich <drabin89@gmail.com>
2016-07-01 17:53:01 -0700

    refactored recController

--- server/controllers/recController.js
+++ server/controllers/recController.js
@@ -2,36 +2,59 @@ const Recipe = require('../models/recipe.js');
 const Ingredient = require('../models/ingredient.js');
 const User = require('../models/user.js');
 const FavRecipes = require('../models/recipe_user.js');
-const each = require('async-each');
+const async = require('async');
 
 module.exports = {
   fetchData: (req, res) => {
-    console.log('hello');
-    let results = {};
     
-    Recipe.fetchAll().then((recipe) => {
-      recipe.models.forEach((rec) => {
-        results[rec.attributes.recipeTitle] = {};
-        FavRecipes.where('recipe_id', rec.attributes.id).fetchAll().then((userRec) => {
-          userRec.models.forEach((users) => {
-            FavRecipes.where('user_id', users.attributes.user_id).fetchAll().then((userFav) => {
-              userFav.forEach((otherRec) => {
-              //console.log(rec.attributes.recipeTitle, results[rec.attributes.recipeTitle][otherRec.attributes.recipeTitle]);
-                if(results[rec.attributes.recipeTitle][otherRec.attributes.recipeTitle] === undefined){
-                  results[rec.attributes.recipeTitle][otherRec.attributes.recipeTitle] = 1;
-                } else {
-                  results[rec.attributes.recipeTitle][otherRec.attributes.recipeTitle] ++;
-
-                }
-                console.log(results);
-            //console.log(rec.attributes.recipeTitle, users.attributes.user_id, otherRec.attributes.recipeTitle);
-              //console.log(results);
-              })
-            });
-          });
-        });
+    const getRecords = (callback) => {
+      let recordsList;
+      FavRecipes.fetchAll().then((records) => {
+        recordsList = records.models;
+        callback(recordsList);
       });
-    });
+    }
+    getRecords((recordsList) => {
+      let results = {};
+      let recipeNames = [];
+      let favRecipes = {};
+      let userFavs = {};
+      recordsList.forEach((record) => {
+        if (results[record.attributes.recipeTitle] === undefined) {
+          results[record.attributes.recipeTitle] = {};
+          results[record.attributes.recipeTitle].id = record.attributes.recipe_id;
+          recipeNames.push(record.attributes.recipeTitle);
+        }
+      });
+      recipeNames.forEach((names) => {
+        for (const key in results) {
+          results[key][names] = 0;
+        }
+      });
+      recordsList.forEach((record) => {
+        if (favRecipes[record.attributes.recipeTitle] === undefined) {
+          favRecipes[record.attributes.recipeTitle] = [];
+          favRecipes[record.attributes.recipeTitle].push(record.attributes.user_id.toString());
+        } else {
+          favRecipes[record.attributes.recipeTitle].push(record.attributes.user_id.toString());
+        }
+      });
+      recordsList.forEach((userRecord) => {
+        if (userFavs[userRecord.attributes.user_id] === undefined) {
+          userFavs[userRecord.attributes.user_id] = [];
+          userFavs[userRecord.attributes.user_id].push(userRecord.attributes.recipeTitle);
+        } else {
+          userFavs[userRecord.attributes.user_id].push(userRecord.attributes.recipeTitle);         
+        }
+      });
+      for (const key in favRecipes) {
+        favRecipes[key].forEach((next) => {
+          userFavs[next].forEach((recipe) => {
+            results[key][recipe] ++;
+          })
+        })
+      }
+      res.send(results);
+    })
   },
-
 };
