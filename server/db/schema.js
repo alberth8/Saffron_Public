8cdf0a87810b34e52ebf3cdc32f5caae93d33ed8
Shane Hubbell <shane.hubbell@gmail.com>
2016-07-05 19:52:19 -0700

    changing env variables back

--- server/db/schema.js
+++ server/db/schema.js
@@ -2,9 +2,9 @@ const knex = require('knex')({
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'saffron',
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.APP,
     charset: 'utf8',
   },
 });
