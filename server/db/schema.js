39b7bdb1294c9ebd124e608fb08ecc2f4110b658
Daniel Rabinovich <drabin89@gmail.com>
2016-07-07 13:41:06 -0700

    Update schema.js

--- server/db/schema.js
+++ server/db/schema.js
@@ -3,8 +3,8 @@ const knex = require('knex')({
   connection: {
     host: '127.0.0.1',
     user: process.env.DB_USER,
-    password: '',
-    database: 'saffron',
+    password: process.env.DB_PW,
+    database: process.env.APP,
     charset: 'utf8',
   },
 });
