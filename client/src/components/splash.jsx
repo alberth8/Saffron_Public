8533f909a538d76c45fc8898c8424079ae1e1627
aezed <aezedr@gmail.com>
2016-07-07 21:32:48 -0700

    Integrate splash page into app

---
+++ client/src/components/splash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => (
+  <div className="container-saffron">
+    <div className="title-saffron">
+      Saffron
+      <div className="subheading">
+        Ingredient Pairing & Recipe Finder
+      </div>
+    </div>
+  </div>
+);
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 import App from './components/App.jsx';
 import Login from './components/login-signup/Login.jsx';
 import Signup from './components/login-signup/Signup.jsx';
@@ -8,9 +8,11 @@ import AddRecipeForm from './components/addRecipeForm/AddRecipeForm.jsx';
 import ProfilePage from './components/userprofile/ProfilePage.jsx';
 import requireAuth from './components/RequireAuth.jsx';
 import IngredientsView from './components/ingredients/IngredientsView.jsx';
+import splash from './components/splash.jsx';
 
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={splash} />
     <Route path="login" component={Login} />
     <Route path="signup" component={Signup} />
     <Route path="profile" component={requireAuth(ProfilePage)} />
